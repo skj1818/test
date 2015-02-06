f3b75eb26323d7c862af77fd215810329eb1e57b
sakaji <sakaji@sakasaka>
2015-02-06 19:03:56 +0900

    gitbub test commit

---
+++ WebViewTest/src/com/example/webviewtest/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.webviewtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView)findViewById(R.id.WebView);
+        webView.loadUrl("http://sakaji-native-dev-tgl10.lv.nndev.net");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+
+    }
+
+}
